a04c4e3217dc87ca76b072eb80403171c7929301
Eugene Alexeenko <yauheni.aliakseyenko@neklo.com>
2015-03-29 19:09:01 +0300

    fix

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var gulp = require('gulp'), // подключаем галп
+    concatCSS = require('gulp-concat-css'), // объединяет css файлы
+    sass = require('gulp-ruby-sass'), // предпроцессор sass
+    minifyCSS = require('gulp-minify-css'), // минифицируем css файлы
+    rename = require('gulp-rename'), // переименовывает файлы
+    notify = require('gulp-notify'), // показывает приятные подсказки
+    prefixer = require('gulp-autoprefixer'), // Плагин префикс
+    livereload = require('gulp-livereload'), //
+    connect = require('gulp-connect'), // создание мини сервера
+    sourcemaps = require('gulp-sourcemaps'); // карта map
+
+// styles
+gulp.task('sass', function() {
+    return sass('sass/main.scss', { compass: true, style: 'expanded', sourcemap: true })
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest('css'))
+        .pipe(rename('main.min.css'))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('css'))
+        .pipe(sourcemaps.write())
+        .pipe(notify({ message: 'Styles task complete styles' }));
+});
+
+// watch
+gulp.task('watch', function() {
+    gulp.watch('sass/**/*.scss', ['sass']); // следим за папкой css используя таск css
+});
+
+// default
+gulp.task('default', ['sass', 'watch']);
+
