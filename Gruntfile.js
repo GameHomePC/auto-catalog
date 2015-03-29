6a7c90f72bda479c4a548312ec9125207aebc7d8
Евгений <ealexseen>
2015-03-29 18:27:33 +0300

    fix

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function(grunt) {
+
+    // 1. Вся настройка находится здесь
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'js/index.js'
+                ],
+                dest: 'js/build/production.js'
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'images/build/'
+                }]
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    livereload: false,
+                    spawn: false
+                }
+            },
+            css: {
+                files: ['sass/*.scss','sass/*/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    livereload: false,
+                    spawn: false
+                }
+            },
+            livereload: {
+                files: ['*.html', 'css/build/global.css', 'js/build/production.min.js'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    compass: true,
+                    style: 'compressed'
+                },
+                files: {
+                    'css/build/global.css': 'sass/global.scss'
+                }
+            }
+        }
+
+    });
+
+    // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'watch', 'sass']);
+
+};
