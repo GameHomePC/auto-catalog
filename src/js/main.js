04d74badfbe95284fedeafe5d6526ac0366c0c9c
Сергей <salexseen@gmail.com>
2015-04-05 17:26:34 +0300

    fix

---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+(function(){
+
+    function App (){}
+
+    App.prototype = {
+
+        select: function(selector){
+
+            function MyJquery(){}
+
+            MyJquery.prototype = {
+                nativeClassList: (document.createElement('div').classList) ? true : false,
+
+                init: function(selector){
+                    this.select = selector;
+                    return this;
+                },
+
+                addClass: function(){
+
+
+
+                },
+                toggleClass: function(){},
+                removeClass: function(){},
+                hasClass: function(clazz){
+
+                    if (this.nativeClassList){
+
+                    } else {
+
+                        var
+                        var reg = new RegExp();
+
+                    }
+
+                }
+
+            };
+
+            return new MyJquery().init(selector);
+
+        }
+
+    };
+
+    var app = new App();
+
+    console.log(app.select(document.body));
+
+
+})();
